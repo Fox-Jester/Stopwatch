d41a2fb814400625ef1de3bedbcec15f650e2d6a
William Marshall <willmarshall2424@gmail.com>
2025-06-12 14:21:44 -0400

    created stopwatch object

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 
 
+
+function Stopwatch(){
+
+    let isCounting = false
+
+    this.duration = 0
+    let startTime, endTime = 0
+    
+
+    this.start = () => {
+        if(!isCounting){
+            startTime = new Date()
+            isCounting = true;
+        }
+        else{
+            return Error("Stopwatch has already started.")
+        }
+        
+    }
+
+    this.stop = () => {
+        if(isCounting){
+            isCounting = false
+            endTime = new Date()
+
+            this.duration = ((endTime - startTime) / 1000);
+
+        }
+    }
+
+
+    this.reset = () => {
+        isCounting = false;
+        this.duration = 0;
+    }
+}
+
+
+
+
+const sw = new Stopwatch()
+
